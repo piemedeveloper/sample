5dfa9298cd9196310e35d19803af81184f356a73
keneikevin <kkevinkenei@gmail.com>
2022-05-11 09:57:38 +0300

    timer_feature

--- app/src/main/java/com/example/testingapi/api/utils/Utility.kt
+++ app/src/main/java/com/example/testingapi/api/utils/Utility.kt
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit
 object Utility {
 
     //time to countdown - 1hr - 60secs
-    const val TIME_COUNTDOWN: Long = 60000L
+    const val TIME_COUNTDOWN: Long = 700000L
     private const val TIME_FORMAT = "%02d:%02d:%02d"
 
 
@@ -13,7 +13,7 @@ object Utility {
     fun Long.formatTime(): String = String.format(
         TIME_FORMAT,
         TimeUnit.MILLISECONDS.toHours(this),
-        TimeUnit.MILLISECONDS.toMinutes(this),
+        TimeUnit.MILLISECONDS.toMinutes(this)% 60,
         TimeUnit.MILLISECONDS.toSeconds(this) % 60
     )
 
